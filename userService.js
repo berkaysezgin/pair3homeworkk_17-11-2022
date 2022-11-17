8c4ad9748c2bfbaa9e3122f0d3e535019d9f51e6
berkaysezgin <88148464+berkaysezgin@users.noreply.github.com>
2022-11-17 19:10:48 +0300

    Add files via upload

---
+++ userService.js
@@ -0,0 +1,30 @@
+import { users } from "./users.js"
+
+export class UserService {
+
+
+    constructor(type) {
+        this.type = type;
+        this.userList = users;
+    }
+
+    add(user) {
+        this.userList.push(user);
+}
+    
+    
+
+    getAll() {
+        return this.userList;
+    }
+
+    getById(id) {
+        let userToFind = this.userList.find(user => user.id === id);
+        return userToFind ?? null;
+    }
+
+    delete(id) {
+        this.userList = this.userList.filter(user => user.id !== id);
+    }
+
+}
